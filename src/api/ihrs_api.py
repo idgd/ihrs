d25af75eb71b18c2a92dfec806cd8255c6175f67
idgd <isaac.dudney@icloud.com>
2018-01-15 15:37:58 -0700

    added 2/3 skeleton functions to api

--- src/api/ihrs_api.py
+++ src/api/ihrs_api.py
@@ -1,7 +1,13 @@
-"""ihrs api"""
+import pymongo
 
 #TODO make database conenction
+def connect():
+	client = pymongo.MongoClient()
+	db = client.local
+	return(db)
 
 #TODO make method for reading records on database
+def read(db,a):
+	return(db.messages.find_one({"_id" : a}))
 
 #TODO possibly make a schema for messages
