31465800bfa06b6b257e55f93472b567a2ecc260
idgd <isaac.dudney@icloud.com>
2018-01-15 15:46:14 -0700

    possibly returning json array for objects now

--- src/api/ihrs_api.py
+++ src/api/ihrs_api.py
@@ -1,4 +1,5 @@
 import pymongo
+import json
 from bson.objectid import ObjectId
 
 #TODO make database conenction
@@ -9,6 +10,6 @@ def connect():
 
 #TODO make method for reading records on database
 def read(db,a):
-	return(db.messages.find_one({"_id" : ObjectId(a)}))
+	return(json.dumps(db.messages.find_one({"_id" : ObjectId(a)})))
 
 #TODO possibly make a schema for messages
