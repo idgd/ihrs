873a3eebc40a723075d58a794cac73bae051034b
idgd <isaac.dudney@icloud.com>
2018-02-10 15:05:26 -0700

    code cleanup, added uc1.2 in the api

--- src/api/ihrs_api.py
+++ src/api/ihrs_api.py
@@ -2,7 +2,10 @@
 # pylint: disable-msg=C0103, C0301
 
 import pymongo
+import json
+import inovonics
 
+from inovonics.echostream.messages.serial import decodeSerialMessage
 from bson.objectid import ObjectId
 
 DATABASE_NAME = "ihrs"
@@ -19,21 +22,27 @@ class DatabaseConnection():
         """connects database to the DATABASE_NAME"""
 
         client = pymongo.MongoClient()
-        databaseName = DATABASE_NAME
-        db = client.get_database(databaseName)
+        database_name = DATABASE_NAME
+        db = client.get_database(database_name)
 
         return db
 
     def read(self, messageID=""):
         """takes in a message ID string and returns the document in the database"""
 
-        messageText = self.db.get_collection(DATABASE_COLLECTION_NAME[0]).find_one({"_id" : ObjectId(messageID)})
+        message_text = self.db.get_collection(DATABASE_COLLECTION_NAME[0]).find_one({"_id" : ObjectId(messageID)})
 
-        return messageText
+        return message_text
 
     def return_list(self):
         """returns the records in the database records"""
 
-        listOfRecords = self.db.get_collection(DATABASE_COLLECTION_NAME[0]).find()
+        list_of_records = self.db.get_collection(DATABASE_COLLECTION_NAME[0]).find()
 
-        return listOfRecords
+        return list_of_records
+
+    def add_message(self,in_bytes):
+        """adds record to the messages collection"""
+
+        json_message = decodeSerialMessage(in_bytes)
+        new_message = self.db.insert(json_message)
