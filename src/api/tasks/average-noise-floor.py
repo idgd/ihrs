65561adf256bb3e82ec9b58539bc8b05506f4b77
Jacob Preston <jacobppreston@gmail.com>
2018-04-04 08:57:42 -0600

    implement average noise floor task

--- src/api/tasks/average-noise-floor.py
+++ src/api/tasks/average-noise-floor.py
@@ -1,6 +1,13 @@
-def calculateAverageNoiseFloor:
+def calculateAverageNoiseFloor(messages):
     ''' documentations '''
         
-    average = 1
+    average = None
+    listOfNoiseFloors = []
+    
+    for index in range(len(messages)):
+        noiseFloor = messages[index].signal.level - messages[index].signal.margin
+        listOfNoiseFloors.append(noiseFloor)
+
+    average = sum(listOfNoiseFloors) / float(len(listOfNoiseFloors))
 
     return average
