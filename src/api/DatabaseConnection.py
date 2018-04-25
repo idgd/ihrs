5794f1ba73909055a16a751e8330ad5e9df86f6e
Jacob Preston <jacobppreston@gmail.com>
2018-04-25 16:41:43 -0600

    remove unused methods

--- src/api/DatabaseConnection.py
+++ src/api/DatabaseConnection.py
@@ -1,7 +1,4 @@
 import pymongo
-import json
-from inovonics.echostream.messages.serial import decodeSerialMessage
-from bson.objectid import ObjectId
 
 DATABASE_NAME = "ihrs"
 DATABASE_COLLECTION_NAME = ["messages"]
@@ -18,16 +15,7 @@ class DatabaseConnection():
 
         return db
 
-    def read(self, messageID=""):
-        message_text = self.db.get_collection(DATABASE_COLLECTION_NAME[0]).find_one({"_id" : ObjectId(messageID)})
-
-        return message_text
-
     def return_list(self):
         list_of_records = self.db.get_collection(DATABASE_COLLECTION_NAME[0]).find()
 
         return list_of_records
-
-    def add_message(self,in_bytes):
-        json_message = decodeSerialMessage(in_bytes)
-        new_message = self.db.insert(json_message)
